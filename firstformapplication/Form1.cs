6cd883fd9c514d29345817ecee586e80a4f4102c
KimYeeun <s2018s01@e-mirim.hs.kr>
2020-05-19 16:48:15 +0900

    21-7. 윈도 폼 - 기본

---
+++ firstformapplication/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace firstformapplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            myButton.Enabled = true;
+            myButton.Text = "코드에서 변경";
+
+            for (int i = 0; i < 5; i++)
+            {
+                Button button = new Button();
+                Controls.Add(button);
+
+                button.Location = new Point(13, 13 + (23+3)*i);
+                button.Text = "동적 생성"+i+"번 째";
+                button.Width = 100;
+            }
+        }
+    }
+}
